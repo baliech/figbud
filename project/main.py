fbee0dd3f71488dc76c69bc72c25a3648e85c732
baliech <123605957+baliech@users.noreply.github.com>
2024-08-13 03:18:12 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -129,9 +129,10 @@ if st.session_state.texts:
             "Find name in the provided text, The name shouldn't be the name of a doctor",
             "What is the invoice number?",
             "What is the invoice date?",
+            "What is the date ?",
             "What is the invoice reference number?note that reference number is not the same as invoice number",
             "What is Co Reg No?",
-            "What is GST Reg No?"
+            "What is GST Reg No?",
             "What is the total?",
             "From the text provided, can you find traditional Chinese medicine or TCM?"
         ],
